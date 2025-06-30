0b59a4ce0261450797ee06703bca4d45da75341a
heega <jsa7868@gmail.com>
2025-06-30 17:29:39 +0900

    sec08

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -13,9 +13,9 @@ import org.springframework.context.annotation.FilterType;
         classes = Configuration.class)
 )
 public class AutoAppConfig {
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 
 }
